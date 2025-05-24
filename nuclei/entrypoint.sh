c6130db07a2a22e41c7a0761069b9b334ad94b55
Missael Cortés Mendoza <60858064+missaelcorm@users.noreply.github.com>
2025-05-24 04:37:27 +0000

    Add nuclei docker compose and templates for waf testing

---
+++ nuclei/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+HEADER_FLAG=""
+
+[[ -z "$HEADER_WAF_BYPASS" ]] && HEADER_FLAG="" || HEADER_FLAG="-H"
+[[ "$DEBUG" == "1" ]] && DEBUG_FLAG="-debug" || DEBUG_FLAG=""
+
+set -x
+nuclei -u $TARGET -w $WORKFLOW $HEADER_FLAG "$HEADER_WAF_BYPASS" $DEBUG_FLAG
+# nuclei -u $TARGET -w $WORKFLOW $HEADER_FLAG "$HEADER_WAF_BYPASS" -ms --omit-raw -omit-template $DEBUG_FLAG
